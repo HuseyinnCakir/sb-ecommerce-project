0fa8fad40bd9d0ed3cbc545eec55bed9f7054b49
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-10-27 00:32:02 +0300

    categoryDto created

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class CategoryController {
 
 
     @GetMapping("/api/public/categories")
-    public ResponseEntity<List<Category>> getAllCategories(){
-        List<Category> allCategories = categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(){
+        CategoryResponse allCategories = categoryService.getAllCategories();
         return new ResponseEntity<>(allCategories,HttpStatus.OK);
     }
 
--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,11 +1,12 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 
 import java.util.List;
 
 public interface CategoryService {
-    List<Category> getAllCategories();
+    CategoryResponse getAllCategories();
     void createdCategory(Category category);
     Category updateCategory(Category category,Long categoryId);
     String deleteCategory(Long categoryId);
