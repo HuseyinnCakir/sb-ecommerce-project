495edd3e4b82c21a13386bd6bf6b2ddc81273608
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-10-30 23:35:16 +0300

    cart and cartItem repository added

---
+++ src/main/java/com/ecommerce/project/repository/CartRepository.java
@@ -0,0 +1,6 @@
+package com.ecommerce.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<CartRepository,Long> {
+}
