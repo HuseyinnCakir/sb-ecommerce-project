1a7b189e7fce7c54657248e784c9a1d6c0fee18c
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-11-01 20:52:40 +0300

    model bugs fixed

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -16,34 +17,44 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
-    @NotBlank
-    @Size(min = 5, message = "Building name must be 5 at least characters")
-    private String buildingName;
-    @NotBlank
-    @Size(min = 3, message = "City name must be 3 at least characters")
-    private String city;
-    @Size(min = 3, message = "Country name must be 3 at least characters")
-    private String country;
-    @Size(min = 6, message = "Pincode name must be 6 at least characters")
-    private String pinCode;
-    @Size(min = 3, message = "State name must be 6 at least characters")
-    private String state;
-    @Size(min = 3, message = "Street name must be 6 at least characters")
-    private String street;
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "addresses")
-    private List<User> user;
 
-    public Address(String buildingName, String city, String country, String pinCode, String state, String street) {
+    @NotBlank
+    @Size(min = 5, message = "Street name must be atleast 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be atleast 5 characters")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 4, message = "City name must be atleast 4 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "State name must be atleast 2 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be atleast 2 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 5, message = "Pincode must be atleast 5 characters")
+    private String pincode;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Address(String street, String buildingName, String city, String state, String country, String pincode) {
+        this.street = street;
         this.buildingName = buildingName;
         this.city = city;
-        this.country = country;
-        this.pinCode = pinCode;
         this.state = state;
-        this.street = street;
+        this.country = country;
+        this.pincode = pincode;
     }
 }
--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @NoArgsConstructor
 @Table(name = "users",
         uniqueConstraints = {
-        @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "email")
         })
 public class User {
@@ -24,10 +24,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long userId;
+
     @NotBlank
     @Size(max = 20)
     @Column(name = "username")
     private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    @Column(name = "password")
+    private String password;
 
     public User(String userName, String email, String password) {
         this.userName = userName;
@@ -35,34 +47,30 @@ public class User {
         this.password = password;
     }
 
-    @NotBlank
-    @Size(max = 50)
-    @Email
-    @Column(name = "email")
-    private String email;
-    @NotBlank
-    @Size(max = 100)
-    @Column(name = "password")
-    private String password;
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},fetch = FetchType.EAGER)
-    @JoinTable(name = "user_role", joinColumns  = @JoinColumn(name = "user_id")
-            ,inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @Setter
+    @Getter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
     @Getter
     @Setter
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},fetch = FetchType.EAGER)
-    @JoinTable(name = "user_address", joinColumns  = @JoinColumn(name = "user_id")
-            ,inverseJoinColumns = @JoinColumn(name = "address_id"))
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+//    @JoinTable(name = "user_address",
+//                joinColumns = @JoinColumn(name = "user_id"),
+//                inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses = new ArrayList<>();
 
-    public User(Set<Role> roles) {
-        this.roles = roles;
-    }
     @ToString.Exclude
-    @OneToOne(mappedBy = "cart",cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REMOVE})
+    @OneToOne(mappedBy = "user", cascade = { CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private Cart cart;
 
     @ToString.Exclude
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE},orphanRemoval = true)
+    @OneToMany(mappedBy = "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            orphanRemoval = true)
     private Set<Product> products;
 }
